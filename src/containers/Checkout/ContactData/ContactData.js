2941e90094ff09700f44437a8af983c28a060cb4
Sorin Dragomir <sorin21us@yahoo.com>
2018-08-15 23:15:18 +0300

    first commit

---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,126 @@
+import React, { Component } from 'react';
+
+import Button from '../../../components/UI/Button/Button';
+import Spinner from '../../../components/UI/Spinner/Spinner';
+import classes from './ContactData.css';
+import axios from '../../../axios-orders';
+import Input from '../../../components/UI/Input/Input'
+
+class ContactData extends Component {
+  state = {
+    orderForm : {
+      name: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your name'
+        },
+        value: ''
+      },
+      street: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Street'
+        },
+        value: ''
+      },
+      zipCode: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'ZipCode'
+        },
+        value: ''
+      },
+      country: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Country'
+        },
+        value: ''
+      },
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'Email'
+        },
+        value: ''
+      },
+      deliveryMethod: {
+        elementType: 'select',
+        elementConfig: {
+          options: [
+            {value: 'fastest', displayValue: 'Fastest'},
+            {value: 'cheapest', displayValue: 'Cheapest'}
+          ]
+        }
+      },
+    },
+    loading: false
+  }
+
+  orderHandler = ( event ) => {
+    event.preventDefault();
+    this.setState( { loading: true } );
+    const order = {
+        ingredients: this.props.ingredients,
+        price: this.props.price,
+        customer: {
+            name: 'Max Schwarzmüller',
+            address: {
+                street: 'Teststreet 1',
+                zipCode: '41351',
+                country: 'Germany'
+            },
+            email: 'test@test.com'
+        },
+        deliveryMethod: 'fastest'
+    }
+    axios.post( '/orders.json', order )
+        .then( response => {
+            this.setState( { loading: false } );
+            this.props.history.push('/');
+        } )
+        .catch( error => {
+            this.setState( { loading: false } );
+        } );
+  }
+
+  render () {
+    const formElementsArray = [];
+    for (let key in this.state.orderForm) {
+      formElementsArray.push({
+        // name, street, etc
+        id: key,
+        // elementType, elementConfig, etc
+        config: this.state.orderForm[key]
+      })
+    }
+    let form = (
+        <form>
+            {formElementsArray.map((formElement) => (
+              <Input 
+                key={formElement.id}
+                elementType={formElement.config.elementType} 
+                elementConfig={formElement.config.elementConfig} 
+                value={formElement.config.value}/>
+            ))}
+            <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+        </form>
+    );
+    if ( this.state.loading ) {
+        form = <Spinner />;
+    }
+    return (
+        <div className={classes.ContactData}>
+            <h4>Enter your Contact Data</h4>
+            {form}
+        </div>
+    );
+  }
+}
+
+export default ContactData;
